1d94fd765b1850962ed81a5ca4abbd19cfe71de7
dolefi <filipdol@email.cz>
2019-11-20 23:21:23 +0100

    working storage, alert when deleting storage, rendering stored news and removed first index entry

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-favourites',
@@ -11,25 +13,50 @@ export class FavouritesPage implements OnInit {
     StoreArray: any = [];
     downloadArray: any = [];
     maxValue: string = "Nothing gotten yet";
-    //ID: number = 0;
 
-    constructor(private storage: Storage) {
+    constructor(private storage: Storage, public alertController: AlertController) {
     }
 
     ngOnInit(): void {
-        this.buttonGet();
+        this.GenerateStoredNews();
     }
 
-    buttonGet() {
+    GenerateStoredNews() {
+        this.StoreArray.length = 0;
         this.storage.forEach((value, key) => {
             this.StoreArray.push(JSON.parse(value));
         }).catch(err => { console.log('errr', err) })
         console.log("created array", this.StoreArray);
-        this.StoreArray.shift();
     }
 
     buttonClear() {
         this.storage.clear();
         console.log("cleared storage");
+    }
+
+    async ClearAlert() {
+        const alert = await this.alertController.create({
+            header: 'Confirmation',
+            message: 'This will permanently delete <strong>all</strong> stored news articles!',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: (blah) => {
+                        console.log('Confirm Cancel: blah');
+                    }
+                }, {
+                    text: 'Okay',
+                    handler: () => {
+                        console.log('Confirm Okay');
+                        this.buttonClear();
+                        this.GenerateStoredNews();
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
     }
 }
